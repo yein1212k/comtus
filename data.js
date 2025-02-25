94282e85f19ab8634efbf492bcbcbdffb8ec6510
펜타브리드_강예인 <167836520+yein1212k@users.noreply.github.com>
2025-02-25 10:10:08 +0900

    Add files via upload

---
+++ data.js
@@ -0,0 +1,18 @@
+const obj = {
+  selected :[
+    {name:"코드1", value:"code1"},
+    {name:"코드2", value:"code2"},
+    {name:"코드3", value:"code3"},
+    {name:"코드4", value:"code4"},
+    {name:"코드5", value:"code5"},
+    {name:"코드6", value:"code6"},
+    {name:"코드7", value:"code7"},
+    {name:"코드8", value:"code8"},
+    {name:"코드9", value:"code9"},
+    {name:"코드10", value:"code10"},
+    {name:"코드11", value:"code11"},
+    {name:"코드12", value:"code12"},
+    {name:"코드13", value:"code13"}
+  ]
+}
+console.log(JSON.stringify(obj));
